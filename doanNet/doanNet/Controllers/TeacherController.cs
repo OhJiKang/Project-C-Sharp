478e7cffd04f7047be6ae6e77a9056cd1190c49f
Ngo Chi Cuong <saofaixoan5464@gmail.com>
2024-02-09 16:39:13 +0700

    Adding some Modal

---
+++ doanNet/doanNet/Controllers/TeacherController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace doanNet.Controllers
+{
+    public class TeacherController : Controller
+    {
+        // GET: Teacher
+        public ActionResult Mistake()
+        {
+            return View();
+        }
+        public ActionResult Contract()
+        {
+            return View();
+        }
+        public ActionResult Post()
+        {
+            return View();
+        }
+    }
+}
