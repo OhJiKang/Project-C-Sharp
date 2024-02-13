7832c2786ccef3460658a30d98d1089f736f22dc
Ngo Chi Cuong <saofaixoan5464@gmail.com>
2024-02-13 21:58:13 +0700

    Config Adding Post

--- doanNet/doanNet/Controllers/TeacherController.cs
+++ doanNet/doanNet/Controllers/TeacherController.cs
@@ -21,5 +21,9 @@ namespace doanNet.Controllers
         {
             return View();
         }
+        public ActionResult AddingPost()
+        {
+            return View();
+        }
     }
 }
