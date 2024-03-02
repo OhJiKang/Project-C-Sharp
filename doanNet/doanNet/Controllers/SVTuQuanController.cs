945f859b1271baceb7d6973fed6446de8fce5f4d
nhuquynh24032003 <158125758+nhuquynh24032003@users.noreply.github.com>
2024-03-03 03:23:13 +0700

    SVTUQUAN

---
+++ doanNet/doanNet/Controllers/SVTuQuanController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace doanNet.Controllers
+{
+    public class SVTuQuanController : Controller
+    {
+        // GET: SVTuQuan
+        public ActionResult LichTrucTuan()
+        {
+            return View();
+        }
+        public ActionResult DangKyLichGacCong()
+        {
+            return View();
+        }
+        public ActionResult GhiLoiKTX()
+        {
+            return View();
+        }
+    }
+}
