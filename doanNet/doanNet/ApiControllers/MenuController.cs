0eb9934196936b54286d9499f6c780302c14a007
Ngo Chi Cuong <saofaixoan5464@gmail.com>
2024-03-03 23:30:21 +0700

    Adding Menu

---
+++ doanNet/doanNet/ApiControllers/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using doanNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace doanNet.ApiControllers
+{
+    public class MenuController : ApiController
+    {
+        KTXTDTUEntities1 db = new KTXTDTUEntities1();
+
+        public List<Menu> GetAllFaculties()
+        {
+            return db.Menus.ToList();
+        }
+    }
+}
