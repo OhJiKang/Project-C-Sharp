9a5712523a6a835b696093f24f5448c42dd3ea6c
Ngo Chi Cuong <saofaixoan5464@gmail.com>
2024-03-04 18:06:56 +0700

    Adding DTO Changing Solution

---
+++ doanNet/doanNet/Controllers/DTO/AccountDTO.cs
@@ -0,0 +1,28 @@
+﻿using doanNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace doanNet.Controllers.DTO
+{
+    public class AccountDTO
+    {
+        public int IDAccount { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public byte[] Available { get; set; }
+        public string Name { get; set; }
+        public int IDSinhVien { get; set; }
+
+        public int AccountID { get; set; }
+
+        public int ContractID { get; set; }
+
+        public int FacultyID { get; set; }
+
+        public List<int> LogsID { get; set; }
+
+        public List<int> MistakesID { get; set; }
+    }
+}
---
+++ doanNet/doanNet/Controllers/DTO/SinhVienDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace doanNet.Controllers.DTO
+{
+    public class SinhVienDTO
+    {
+        public int IDSinhVien { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public System.DateTime BirthDay { get; set; }
+        public System.DateTime AttendDate { get; set; }
+        public int AttendYear { get; set; }
+        public int GraduateYear { get; set; }
+        public string MSSV { get; set; }
+        public int IDFalcuty { get; set; }
+        public int IDContract { get; set; }
+        public int IDRoom { get; set; }
+
+        public int IDAccount { get; set; }
+    }
+}
