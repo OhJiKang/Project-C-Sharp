8c0149157eb367181eef17498e4ba58432ec57ed
nhuquynh240303 <100460538+nhuquynh240303@users.noreply.github.com>
2024-04-05 11:53:45 +0700

    add areas

---
+++ doanNet/doanNet/Areas/GiaoVien/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace doanNet.Areas.GiaoVien.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: GiaoVien/Default
+        public ActionResult ChoosingRoom()
+        {
+            return View();
+        }
+    }
+}
