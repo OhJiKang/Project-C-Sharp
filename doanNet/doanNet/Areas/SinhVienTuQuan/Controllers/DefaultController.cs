2273732cf819d3e857d204556149fc6aca6795ac
nhuquynh240303 <100460538+nhuquynh240303@users.noreply.github.com>
2024-04-07 16:44:29 +0700

    areas

---
+++ doanNet/doanNet/Areas/SinhVienTuQuan/Controllers/DefaultController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace doanNet.Areas.SinhVienTuQuan.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: SinhVienTuQuan/Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult LichTrucTuan()
+        {
+            return View();
+        }
+        public ActionResult DangKyLichGacCong()
+        {
+            return View();
+        }
+        public ActionResult GhiLoiKTX()
+        {
+            return View();
+        }
+        public ActionResult DiemDanhKTX()
+        {
+            return View();
+        }
+    }
+}
